129561430bf57634bdddd7db9675f999d53fd430
andkymon <100330163+andkymon@users.noreply.github.com>
2024-12-19 11:25:34 +0800

    Add task/project property validation with get/set
    - Set the privacy of Task and Project properties using private fields.
    - Added getters and setters to validate property changes using the
    InputValidator class.

--- src/index.js
+++ src/index.js
@@ -1,22 +1,100 @@
 import css from "./style.css"
 
 class Task {
+    #name;
+    #description;
+    #dueDate;
+    #projectName;
+    #isPriority;
+    #isComplete;
+
     constructor(name, description, dueDate, projectName, isPriority, isComplete) {
-        this.name = name;
-        this.description = description;
-        this.dueDate = dueDate;
-        this.projectName = projectName;
-        this.isPriority = isPriority;
-        this.isComplete = isComplete;
+        this.#name = name;
+        this.#description = description;
+        this.#dueDate = dueDate;
+        this.#projectName = projectName;
+        this.#isPriority = isPriority;
+        this.#isComplete = isComplete;
+    }
+
+    get name() {
+        return this.#name;
+    }
+    set name(str) {
+        if (InputValidator.validateName(str) === false) {
+            return;
+        }
+        this.#name = str;
+    }
+
+    get description() {
+        return this.#description;
+    }
+    set description(str) {
+        if (InputValidator.validateDescription(str) === false) {
+            return;
+        }
+        this.#description = str;
+    }
+
+    get dueDate() {
+        return this.#dueDate;
+    }
+    set dueDate(date) {
+        if (InputValidator.validateDate(date) === false) {
+            return;
+        }
+        this.#dueDate = date;
+    }
+
+    get projectName() {
+        return this.#projectName;
+    }
+    set projectName(str) {
+        if (InputValidator.validateName(str) === false) {
+            return;
+        }
+        this.#projectName = str;
+    }
+
+    get isPriority() {
+        return this.#isPriority;
+    }
+    set isPriority(bool) {
+        if (InputValidator.validateBoolean(bool) === false) {
+            return;
+        }
+        this.#isPriority = bool;
+    }
+
+    get isComplete() {
+        return this.#isComplete;
+    }
+    set isComplete(bool) {
+        if (InputValidator.validateBoolean(bool) === false) {
+            return;
+        }
+        this.#isComplete = bool;
     }
 }
 
 class Project {
+    #name;
     constructor(name) {
-        this.name = name;
+        this.#name = name;
     }
 
     tasks = [];
+
+    get name() {
+        return this.#name;
+    }
+    set name(name) {
+        if (InputValidator.validateName(name) === false) {
+            return;
+        }
+        this.#name = name;
+    }
 }
 
 class ToDoStorage {
@@ -34,6 +112,7 @@ class ToDoStorage {
         }
         this.projects[projectNames.indexOf(projectName)].tasks.push(new Task(name, description, dueDate, projectName, isPriority, isComplete));
     }
+
     static addProject(name) {
         if (InputValidator.validateName(name) === false) {
             return;
@@ -93,5 +172,10 @@ class InputValidator {
     }
 }
 
-ToDoStorage.addTask("ky", "hanz", "12-01-2000", "poo", true, true);
+ToDoStorage.addTask("ky", "hanz", "12-01-2000", "pooproject", true, true);
+ToDoStorage.projects[0].name = "1234567890123456";
+ToDoStorage.projects[0].name = "1234123456789012345612345678901234561234567890123456567890123456123456789012345612345678901234561234567890123456";
+ToDoStorage.projects[0].tasks[0].name = "1234123456789012345612345678901234561234567890123456567890123456123456789012345612345678901234561234567890123456";
+ToDoStorage.projects[0].tasks[0].name = "kyle";
+
 console.log(ToDoStorage.projects);
