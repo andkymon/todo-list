72f8523b5673b9ebc919ff7a5735743caa68b6dd
andkymon <100330163+andkymon@users.noreply.github.com>
2024-12-23 10:46:57 +0800

    feat: add funcs for adding and displaying projects
    - Add event listeners for showing and hiding the add project dialog
    - Add event listeners for adding a new project
    - Add functionality to clear and refresh project list display

--- src/index.js
+++ src/index.js
@@ -3,34 +3,14 @@ import { ToDoStorage } from './logic/ToDoStorage.js';
 import { ProjectFilter } from './logic/ProjectFilter.js';
 import { ProjectSort } from './logic/ProjectSort.js';
 
-// Example usage
-ToDoStorage.addProject("A");
-ToDoStorage.addProject("B");
-ToDoStorage.addProject("C");
-
-ToDoStorage.addTask("A", "Code for 8 hours", "12-21-2024", true, true, 0);
-ToDoStorage.addTask("B", "Eat yummy food", "12-20-2024", false, true, 0);
-ToDoStorage.addTask("X", "Eat yummy food", "12-24-2024", true, false, 0);
-ToDoStorage.addTask("Y", "Eat yummy food", "12-23-2024", false, true, 0);
-ToDoStorage.addTask("Z", "Eat yummy food", "12-28-2024", true, true, 0);
-ToDoStorage.addTask("C", "Sleep for 8 hours", "12-23-2024", true, false, 0);
-ToDoStorage.addTask("D", "Sleep for 8 hours", "12-30-2024", true, false, 1);
-
-ToDoStorage.addTask("E", "Code for 8 hours", "12-21-2024", true, true, 1);
-ToDoStorage.addTask("F", "Sleep for 8 hours", "12-30-2024", true, false, 2);
-ToDoStorage.addTask("G", "Sleep for 8 hours", "12-30-2024", true, false, 1);
-
-console.log(ToDoStorage.projects[0]);
-console.log(ProjectSort.sortByCompletion(ToDoStorage.projects[0]));
-console.log(ProjectFilter.filterImportantTasks(ToDoStorage.projects[0]));
-
+//Nav Toggle
 const navToggle = document.querySelector("#nav-toggle");
 const body = document.querySelector("body")
 const nav = document.querySelector("nav");
 body.style.gridTemplateColumns = "360px 1fr";
 nav.style.transform = "none";
+
 function toggle() {
-    console.log(body.style.gridTemplateColumns)
     if (body.style.gridTemplateColumns === "360px 1fr") {
         body.style.gridTemplateColumns = "0px 1fr";
         nav.style.transform = "translateX(-360px)";
@@ -41,6 +21,67 @@ function toggle() {
 }
 
 navToggle.addEventListener("click", toggle);
+
+//Add Project Dialog
+const addProjectBtn = document.querySelector("#add-project");
+const addProjectDialog = document.querySelector("#project-dialog");
+function showAddProjectDialog() {
+    addProjectDialog.showModal();
+}
+addProjectBtn.addEventListener("click", showAddProjectDialog);
+
+const closeBtn = document.querySelector(".close");
+function hideAddProjectDialog() {
+    addProjectDialog.close();
+}
+closeBtn.addEventListener("click", hideAddProjectDialog);
+
+//Add Project Button
+const projectConfirmBtn = document.querySelector("#project-dialog .confirm");
+const projectInputName = document.querySelector("#project-input-name");
+
+function addProject() {
+    const name = projectInputName.value;
+    ToDoStorage.addProject(name);
+    projectInputName.value = "";
+    console.log(ToDoStorage.projects);
+    updateProjects();
+}
+projectConfirmBtn.addEventListener("click", addProject);
+
+//Display Projects
+function updateProjects() {
+    clearProjects();
+    for (const project of ToDoStorage.projects) {
+        const navBtn = document.createElement("button");
+        navBtn.classList.add("nav-btn");
+        navBtn.textContent = project.name;
+
+        const deleteBtn = document.createElement("button");
+        deleteBtn.classList.add("small-btn", "delete");
+
+        const btnWrapper = document.createElement("div");
+        btnWrapper.classList.add("btn-wrapper");
+
+        btnWrapper.append(navBtn, deleteBtn);
+        nav.append(btnWrapper);
+    }
+}
+
+//Clear Projects
+function clearProjects() {
+    const navBtns = document.querySelectorAll("nav .btn-wrapper");
+    for (const navBtn of navBtns) {
+        if (navBtn.id === "all") {
+            continue;
+        }
+        navBtn.remove();
+    }
+}
+
+
+
+
 
 
 
