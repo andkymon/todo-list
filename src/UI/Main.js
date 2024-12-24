534d1a5e2ce60898e8f57970fb88dc18380cef95
andkymon <100330163+andkymon@users.noreply.github.com>
2024-12-24 14:12:46 +0800

    feat: implement Main class to manage Main Display
    - Create `Main` class to handle the Main Display
    - Add `updateTasks` method to display tasks based on the selected
    project index:
      - If a valid index is passed, display tasks from the corresponding
      project in `ToDoStorage.projects`
      - If -1 is passed, display tasks from all existing projects
    - Implement `#clearTask` method to clear all tasks from the screen, used
    in `updateTasks` to reset the display
    - Add `#addTask` method to generate and append DOM elements for tasks
    based on their name and due date

---
+++ src/UI/Main.js
@@ -0,0 +1,63 @@
+import { ToDoStorage } from '../logic/ToDoStorage.js';
+
+export class Main {
+    static updateTasks(projectIndex) {
+        this.#clearTask();
+        if (projectIndex === -1) { //For "all" NavBtn
+            for (const project of ToDoStorage.projects) {
+                for (const task of project.tasks) {
+                    this.#addTask(task.name, task.dueDate);
+                }
+            }       
+        } else {
+            for (const task of ToDoStorage.projects[projectIndex].tasks) {
+                this.#addTask(task.name, task.dueDate);
+            }
+        }
+    }
+    static #clearTask() {
+        const tasks = document.querySelectorAll(".task");
+        for (const task of tasks) {
+            task.remove();
+        }
+    }
+    static #addTask(name, dueDate) {
+        /*
+        <div class="task">
+            <button class="checkbox"></button>
+            <span class="task-name">Task A</span>
+            <span class="due-date">12-01-2000</span>
+            <button class="small-btn task-info"></button>
+            <button class="small-btn important clicked"></button>
+            <button class="small-btn edit"></button>
+            <button class="small-btn delete"></button>
+        </div>
+        */
+        const main = document.querySelector("main");
+        const taskDiv = document.createElement("div");
+        const taskElements = [];
+        const btnClassList = ["checkbox", "task-info", "important", "edit", "delete", "small-btn"];
+        const spanClassList = ["task-name", "due-date"];
+        const spanTextContent = [name, dueDate.toDateString()];
+    
+        for (let i = 0; i < 5; i++) {
+            const btn = document.createElement("button");
+            if (i != 0) { //Add "small-btn" class to all buttons except the first
+                btn.classList.add(btnClassList[btnClassList.length - 1]);
+            }
+            btn.classList.add(btnClassList[i]);
+            taskElements.push(btn);
+        }
+        for (let i = 0; i < 2; i++) {
+            const span = document.createElement("span");
+            span.classList.add(spanClassList[i]);
+            span.textContent = spanTextContent[i];
+            taskElements.splice(i + 1, 0, span); //Add span to index 1 and 2 of taskElements
+        }
+        for (const taskElement of taskElements) {
+            taskDiv.append(taskElement);
+        }
+        taskDiv.classList.add("task");
+        main.append(taskDiv);
+    }
+}
