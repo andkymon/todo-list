445c776d7bf50f233e0a7d8ae46ee2571eb850fb
andkymon <100330163+andkymon@users.noreply.github.com>
2024-12-29 19:56:28 +0800

    Add space under class field declaration

--- src/logic/Project.js
+++ src/logic/Project.js
@@ -2,6 +2,7 @@ import { InputValidator } from './InputValidator.js';
 
 export class Project {
     #name;
+
     constructor(name) {
         this.#name = name;
     }
@@ -11,6 +12,7 @@ export class Project {
     get name() {
         return this.#name;
     }
+    
     set name(name) {
         if (InputValidator.validateName(name) === false) {
             return;
