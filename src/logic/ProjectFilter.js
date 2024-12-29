1215dba7006dfc65fc90eabbf0c746e1e9e17978
andkymon <100330163+andkymon@users.noreply.github.com>
2024-12-29 20:02:24 +0800

    Refactor ProjectFilter to be object not class

--- src/logic/ProjectFilter.js
+++ src/logic/ProjectFilter.js
@@ -1,36 +1,44 @@
-export class ProjectFilter {
-    static #weekms = 604800000; //week in milliseconds
-    static #todayDate = new Date((new Date()).toDateString()); //toDateString to set time to 12 midnight of the current day
-    static #nextWeekDate = new Date(this.#todayDate.getTime() + this.#weekms);
-	    
-    static filterTasksToday(project) {
-        const projectCopy = {...project};
-	    projectCopy.tasks = project.tasks.filter((task) => task.dueDate.getTime() === this.#todayDate.getTime());
-	    return projectCopy;
-    }
+export const ProjectFilter = (function() {
+    const weekms = 604800000; // Week in milliseconds
+    const todayDate = new Date((new Date()).toDateString()); // toDateString to set time to 12 midnight of the current day
+    const nextWeekDate = new Date(todayDate.getTime() + weekms);
 
-    static filterTasksThisWeek(project) {
-        const projectCopy = {...project};
-	    projectCopy.tasks = project.tasks.filter((task) => (task.dueDate >= this.#todayDate && task.dueDate < this.#nextWeekDate));
-	    return projectCopy;
-    }
-	
-    static filterImportantTasks(project) {
-        const projectCopy = {...project};
-	    projectCopy.tasks = project.tasks.filter((task) => task.isPriority === true);
-	    return projectCopy;
-    }
-
-    static filterIncompleteTasks(project) {
-        const projectCopy = {...project};
-	    projectCopy.tasks = project.tasks.filter(task => task.isComplete === false);
-	    return projectCopy;
-    }
-
-    static filterOverdueTasks(project) {
-        const projectCopy = {...project};
-        const dateToday = new Date(new Date().toDateString());
-	    projectCopy.tasks = project.tasks.filter(task => task.dueDate < dateToday === true);
+    function filterTasksToday(project) {
+        const projectCopy = { ...project };
+        projectCopy.tasks = project.tasks.filter((task) => task.dueDate.getTime() === todayDate.getTime());
         return projectCopy;
     }
-}
+
+    function filterTasksThisWeek(project) {
+        const projectCopy = { ...project };
+        projectCopy.tasks = project.tasks.filter((task) => task.dueDate >= todayDate && task.dueDate < nextWeekDate);
+        return projectCopy;
+    }
+
+    function filterImportantTasks(project) {
+        const projectCopy = { ...project };
+        projectCopy.tasks = project.tasks.filter((task) => task.isPriority === true);
+        return projectCopy;
+    }
+
+    function filterIncompleteTasks(project) {
+        const projectCopy = { ...project };
+        projectCopy.tasks = project.tasks.filter((task) => task.isComplete === false);
+        return projectCopy;
+    }
+
+    function filterOverdueTasks(project) {
+        const projectCopy = { ...project };
+        const dateToday = new Date(new Date().toDateString());
+        projectCopy.tasks = project.tasks.filter((task) => task.dueDate < dateToday);
+        return projectCopy;
+    }
+
+    return {
+        filterTasksToday,
+        filterTasksThisWeek,
+        filterImportantTasks,
+        filterIncompleteTasks,
+        filterOverdueTasks
+    };
+})();
