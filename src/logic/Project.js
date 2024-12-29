820c597c9e65da27f3a0692230094dc298342cb8
andkymon <100330163+andkymon@users.noreply.github.com>
2024-12-29 19:57:21 +0800

    Remove space between getter/setter

--- src/logic/Project.js
+++ src/logic/Project.js
@@ -2,7 +2,7 @@ import { InputValidator } from './InputValidator.js';
 
 export class Project {
     #name;
-
+    
     constructor(name) {
         this.#name = name;
     }
@@ -12,7 +12,6 @@ export class Project {
     get name() {
         return this.#name;
     }
-    
     set name(name) {
         if (InputValidator.validateName(name) === false) {
             return;
