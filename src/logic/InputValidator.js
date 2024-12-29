96d276f3860a3fd0aeae01f702297460ae154e61
andkymon <100330163+andkymon@users.noreply.github.com>
2024-12-29 19:58:25 +0800

    Refactor InputValidator to be an object not class

--- src/logic/InputValidator.js
+++ src/logic/InputValidator.js
@@ -1,51 +1,67 @@
-export class InputValidator {
-    static validateName(name) {
+export const InputValidator = (function() {
+    function validateName(name) {
         const nameStr = name.toString();
         if (nameStr === "" || nameStr.length > 50) {
             console.error("Input must be 1 to 50 characters");
             return false;
         }
+        return true;
     }
 
-    static validateDescription(description) {
+    function validateDescription(description) {
         const descriptionStr = description.toString();
         if (descriptionStr.length > 255) {
             console.error("Input must be 0 to 255 characters");
             return false;
         }
+        return true;
     }
 
-    static validateDate(date) {
+    function validateDate(date) {
         if (isNaN(date)) {
             console.error("Input format must be MM-DD-YYYY and valid");
-			return false
+            return false;
         }
+        return true;
     }
 
-    static validateBoolean(bool) {
+    function validateBoolean(bool) {
         if (typeof bool !== "boolean") {
             console.error("Input must be of boolean data type");
             return false;
         }
+        return true;
     }
 
-    static validateList(index, total) {
+    function validateList(index, total) {
         if (!Number.isInteger(index) || index < 0 || index >= total) {
             console.error("Invalid list item selected");
             return false;
         }
+        return true;
     }
 
-    static validateTask(name, description, dueDate, projectIndex, totalProjects) {
-        const inputs = [this.validateName(name), 
-                        this.validateDescription(description),
-                        this.validateDate(dueDate),
-                        this.validateList(projectIndex, totalProjects)
-                        ];
+    function validateTask(name, description, dueDate, projectIndex, totalProjects) {
+        const inputs = [
+            validateName(name), 
+            validateDescription(description),
+            validateDate(dueDate),
+            validateList(projectIndex, totalProjects)
+        ];
         for (const input of inputs) {
             if (input === false) {
                 return false;
             }
         }
+        return true;
     }
-}
+
+    return {
+        validateName,
+        validateDescription,
+        validateDate,
+        validateBoolean,
+        validateList,
+        validateTask
+    };
+})();
