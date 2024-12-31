3476e0f5ff3c9b835a360dbde70ea91e6745e8b3
andkymon <100330163+andkymon@users.noreply.github.com>
2024-12-31 11:14:54 +0800

    Refactor ProjectDialog module
    - Added `addProject` function to handle project name input and
    validation:
        - Integrated `ToDoStorage.addProject` to store projectusing the name
        input, with error handling for invalid names.
        - Reset the input field and hide the dialog after successful project
        creation.
        - Updated `NavBar.updateProjectDisplay` to refresh the project list
        dynamically after project add.
    - It is an instance specific function for the dialog confirm event.
    - Returns a dialog object instance using the Dialog class, passing in
    the project dialog selector, project dialog opener selector, and the
    confirm event handler

--- src/UI/ProjectDialog.js
+++ src/UI/ProjectDialog.js
@@ -1,72 +1,34 @@
+import { Dialog } from './Dialog.js';
 import { ToDoStorage } from '../Logic/ToDoStorage.js';
 import { InvalidStyling } from './InvalidStyling.js';
 import { NavBar } from './NavBar.js';
 
-export class ProjectDialog {
-    static #projectDialog = document.querySelector("#project-dialog");
-    static #transitionTime = 300; //animation transition time in ms
+export const ProjectDialog = (function () {
+    const dialog = new Dialog("#project-dialog", "#add-project", addProject);
 
-    static #showProjectDialog = () => {
-        this.#projectDialog.showModal();
-        setTimeout(() => { //Wait for dialog to open before playing animation
-            this.#projectDialog.classList.add("open");
-        }, 1);
-    }
-
-    static #hideProjectDialog = () => {
-        InvalidStyling.clearInvalidStyles();
-        this.#projectDialog.classList.remove("open");
-        setTimeout(() => { //Wait for animation to play before closing dialog
-            this.#projectDialog.close();
-        }, this.#transitionTime);
-    }
-
-    static #addProject = () => {
-        const projectInputName = document.querySelector("#project-input-name"); 
-        const nav = document.querySelector("nav");
-
-        const name = projectInputName.value;
+    function addProject() {
+        const projectNameInput = document.querySelector("#project-name-input"); 
+        const name = projectNameInput.value;
         const result = ToDoStorage.addProject(name);
+
         if (result === false) {
-            InvalidStyling.showValidationError(projectInputName, "name");
+            InvalidStyling.showValidationError(projectNameInput, "name");
             return;
         }
-        projectInputName.value = "";
-        this.#hideProjectDialog();
+        projectNameInput.value = "";
+        dialog.hideDialog();
         NavBar.updateProjectDisplay(); 
-        //Transition
-        nav.lastChild.classList.add("removed");
-        setTimeout(() => { //Wait for nav.lastChild to be set to the left non-visible area for 1ms before transitioning
-            nav.lastChild.classList.remove("removed");;
-        }, 1);
     }
+
+    return dialog;
+})();
+   
     
-    static init() {
-        this.#projectDialog.style.transition = `${this.#transitionTime}ms`;
+    
 
-        const addProjectBtn = document.querySelector("#add-project");
-        addProjectBtn.addEventListener("click", this.#showProjectDialog);
 
-        const closeBtn = document.querySelector(".close");
-        closeBtn.addEventListener("click", this.#hideProjectDialog);
 
-        //Prevent default behavior of escape key
-        this.#projectDialog.addEventListener("cancel", (event) => {
-            event.preventDefault();
-            this.#hideProjectDialog();
-        });
-        
-        const projectConfirmBtn = document.querySelector("#project-dialog .confirm"); 
-        projectConfirmBtn.addEventListener("click", this.#addProject);
 
-        //Prevent default behavior of enter key
-        this.#projectDialog.addEventListener("keypress", (event) => {
-            if (event.key === "Enter") {
-                event.preventDefault();
-                this.#addProject();
-            }
-        });
-    }
-}
+
 
 
