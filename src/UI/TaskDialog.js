7b0e6a34a56aba971ac5b00caed8b945c3d6ef64
andkymon <100330163+andkymon@users.noreply.github.com>
2024-12-31 11:51:16 +0800

    Refactor TaskDialog module
    - Implemented the `TaskDialog` module, initializing the dialog with
    the appropriate selectors and a confirm action (addTask).
    - Defined the `addTask` function to handle the task creation process
    on confirm event:
      - Retrieved the selected project index from
      `NavBar.getSelectedProjectIndex()`.
      - Accessed input values for task name, description, and due date from
      the DOM.
      - Called `ToDoStorage.addTask` to attempt adding the task, passing the
      input values and project index.
      - Added validation for name, description, and date using
      `InputValidator` and showed error messages via
      `InvalidStyling.showValidationError` if validation failed.
      - If task creation is successful, cleared the inputs using
      `dialog.clearInputs()` and hid the dialog using `dialog.hideDialog()`.
      - Updated the task display by calling `Main.updateTaskDisplay` with
      the selected project.

--- src/UI/TaskDialog.js
+++ src/UI/TaskDialog.js
@@ -1,37 +1,22 @@
+import { Dialog } from './Dialog.js';
 import { ToDoStorage } from '../Logic/ToDoStorage.js';
 import { InputValidator } from '../Logic/InputValidator.js';
 import { InvalidStyling } from './InvalidStyling.js';
 import { Main } from './Main.js';
 import { NavBar } from './NavBar.js';
 
-export class TaskDialog {
-    static #taskDialog = document.querySelector("#task-dialog");
-    static #transitionTime = 300; //animation transition time in ms
+export const TaskDialog = (function () {
+    const dialog = new Dialog("#task-dialog", "#add-task", addTask);
 
-    static #showTaskDialog = () => {
-        this.#taskDialog.showModal();
-        setTimeout(() => { //Wait for dialog to open before playing animation
-            this.#taskDialog.classList.add("open");
-        }, 1);
-    }
-
-    static #hideTaskDialog = () => {
-        InvalidStyling.clearInvalidStyles();
-        this.#taskDialog.classList.remove("open");
-        setTimeout(() => { //Wait for animation to play before closing dialog
-            this.#taskDialog.close();
-        }, this.#transitionTime);
-    }
-
-    static #addTask = () => {
+    function addTask() {
         const projectIndex = NavBar.getSelectedProjectIndex();
-        const taskInputName = document.querySelector("#task-input-name"); 
-        const taskInputDescription = document.querySelector("#task-input-description"); 
-        const taskInputDate = document.querySelector("#task-input-date"); 
+        const taskNameInput = document.querySelector("#task-name-input"); 
+        const taskDescriptionInput = document.querySelector("#task-description-input"); 
+        const taskDateInput = document.querySelector("#task-date-input"); 
 
-        const name = taskInputName.value;
-        const description = taskInputDescription.value;
-        const dueDate = new Date(taskInputDate.valueAsNumber);
+        const name = taskNameInput.value;
+        const description = taskDescriptionInput.value;
+        const dueDate = new Date(taskDateInput.valueAsNumber); //Convert to ms to instantiate a Date object 
 
         const result = ToDoStorage.addTask(name, description, dueDate, projectIndex);
         if (result === false) {
@@ -46,39 +31,9 @@ export class TaskDialog {
             }
             return;
         }
-        taskInputName.value = "";
-        taskInputDescription.value = "";
-        taskInputDate.value = "";
-        this.#hideTaskDialog();
+        //This is invoked here because it should only execute when this function doesn't return early
+        dialog.clearInputs();
+        dialog.hideDialog();
         Main.updateTaskDisplay(projectIndex); 
     }
-    
-    static init() {
-        this.#taskDialog.style.transition = `${this.#transitionTime}ms`;
-
-        const addTaskBtn = document.querySelector("#add-task");
-        addTaskBtn.addEventListener("click", this.#showTaskDialog);
-
-        const closeBtn = document.querySelector("#task-dialog .close");
-        closeBtn.addEventListener("click", this.#hideTaskDialog);
-
-        //Prevent default behavior of escape key
-        this.#taskDialog.addEventListener("cancel", (event) => {
-            event.preventDefault();
-            this.#hideTaskDialog();
-        });
-
-        const taskConfirmBtn = document.querySelector("#task-dialog .confirm"); 
-        taskConfirmBtn.addEventListener("click", this.#addTask);
-
-        //Prevent default behavior of enter key
-        this.#taskDialog.addEventListener("keypress", (event) => {
-            if (event.key === "Enter") {
-                event.preventDefault();
-                this.#addTask();
-            }
-        });
-    }
-}
-
-
+})();
